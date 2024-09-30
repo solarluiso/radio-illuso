16e9d2214b46e0d5cefcd2e41eebaa1d1a3d80c7
Luis <solarluiso@gmail.com>
2024-09-30 10:47:15 -0400

    update

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import MainTextSection from "@/components/MainTextSection";
 import About from "@/components/About";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
       <MainTextSection />
+      {/* <Footer /> */}
       {/* <About /> */}
     </>
   );
--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="flex absolute font-mono text-accent/80 z-20 items-center justify-between mx-auto w-full h-fit lg:px-10 md:px-8 px-6 lg:py-10 py-5">
       <div className="cursor-pointer">
-        <h2 className="text-xl font-bold">PLTU</h2>
+        <h2 className="text-xl font-bold">EnUnCanal</h2>
       </div>
       <div className="flex gap-x-6 sm:gap-x-10 text-xs sm:text-sm lg:text-base uppercase">
         <div className="cursor-pointer hover:text-accent-hover">
